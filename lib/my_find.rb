e90877191999194b6936cdaf675f4defa46652a7
Annabel Wilmerding <awilmerding@Annabel-Wilmerdings-MB-Pro.local>
2021-01-14 10:02:59 -0800

    commit tree 5a6bd7578ac514c300e9772d6be3c2f941aca102

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,25 @@
 require 'pry'
 
 def my_find(collection)
-
+  i = 0
+  while i < collection.length
+    if yield(collection[i])
+        return collection[i]
+    end
+    i+=1
+  end
 end
+
+
+
+# if 0
+#   puts "hey"
+# end
+#
+# truthy
+# falsey
+#   false
+#   nil
+
+
+# [1,2,3,4,5].find {|num| num.even?} => 2
